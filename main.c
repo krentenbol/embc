364b2c5c86956395687eb60b0582a44703249505
Lars <lars.leguijt@hva.nl>
2014-03-23 19:42:17 +0100

    BasisProject

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Lars
+ *
+ * Created on 23 March 2014, 15:17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
