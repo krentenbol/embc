51707e7ba085125cabe611429439886dcb5fe9b2
Krentenbol <>
2014-03-23 19:45:14 +0100

    eerste commit opdr10

---
+++ opdr10.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   opdr10.c
+ * Author: Lars
+ *
+ * Created on 23 March 2014, 19:44
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
